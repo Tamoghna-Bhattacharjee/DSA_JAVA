28ce63f4a31f9d6fe7da077bb3cf75f70e7d5350
Tamoghna-Bhattacharjee <tamoghnabhattacharjee.99@gmail.com>
2021-10-10 22:41:41 +0530

    change

--- src/String_algo/DP/LongestRepeatingSubsequence.java
+++ src/String_algo/DP/LongestRepeatingSubsequence.java
@@ -15,7 +15,7 @@ public class LongestRepeatingSubsequence {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s.charAt(i) == s.charAt(j) && i != j)
-                    dp[i][j] = Math.max(dp[i][j], 1 + dp[i-1][j-1]);
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
